14cc72e56168a6ba3d28b466a93e81ee8bbe84fd
ghufran <mohdg6619@gmail.com>
2022-12-24 02:30:43 +0530

    api

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+mongoose.set('strictQuery', false);
 const app = express();
-
+const moment = require('moment')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
@@ -14,13 +16,18 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
+
 app.use (
     function (req, res, next) {
         console.log ("inside GLOBAL MW");
+        // const date=new Date();
+        const date = moment().format("DD-MM-YYYY,hh:mm:ss")
+        const ip = req.ip
+        const hitRoute= req.originalUrl
+        console.log(date,ip,hitRoute) 
         next();
   }
   );
-
 app.use('/', route);
 
 
