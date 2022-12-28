fe2e591c6d6264c73a3403f12b7c88db39d805b9
ghufran <mohdg6619@gmail.com>
2022-12-28 22:45:36 +0530

    JWT-auth-2

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,24 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken")
 
+exports.authenticate = function(req, res, next) {
+    let header = req.headers['x-auth-token']
+   if(!header)return res.send({status :false  , message :"headers is required" })
+
+   jwt.verify(header  , "californium batch" , (err ,decode )=>{
+    if(err)return res.send({status : true , message : err.message })
+    req.id  =decode.userId
     next()
+   })
+ 
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
+
+exports.authorise = function(req, res, next) {
+      let userId = req.id 
+      let id = req.params.userId
+      console.log(userId , id)
+      if(!id)return res.send({status : false , message: "params id required"})
+      if(userId !== id)return res.send({status : false , message: "  unAuthorized"})
+     next()
 }
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -14,8 +14,26 @@ const userSchema = new mongoose.Schema( {
         type: String,
         enum: ["male", "female", "other"]
     },
+    isDeleted:{
+        type: Boolean,
+        default: false
+    },
     age: Number,
     posts: {type: [], deafult: []}
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema)
+
+
+// "firstName" : "Sabiha",
+// "lastName" : "Khan",
+// "mobile" : "9898909087",
+// "emailId" : "sk@gmail.com",
+// "password" : "password123",
+// "gender" : "female",
+// "isDeleted": false, //default value is false 
+// "age" : 12,
+// "createdAt" : ISODate("2022-03-08T05:04:18.737Z"),
+// "updatedAt" : ISODate("2022-03-08T05:04:18.737Z"),
+// "__v" : 0
+// }
